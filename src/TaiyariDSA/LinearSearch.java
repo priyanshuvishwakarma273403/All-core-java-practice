883871bb4c819f7487cbaba0a3efd097f1344b12
Priyanshu <pv254424@gmail.com>
2026-03-29 23:15:21 +0530

    linear search

---
+++ src/TaiyariDSA/LinearSearch.java
@@ -0,0 +1,17 @@
+package TaiyariDSA;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int [] arr = {10,2,4,5,8,4,2};
+        int found = 19;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==found){
+                System.out.println("Element found at index "+i+" at position "+found);
+            }
+            else{
+                System.out.println("Element not found ");
+            }
+            return;
+        }
+    }
+}
