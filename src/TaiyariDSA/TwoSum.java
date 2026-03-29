aa94fdce362b5bb41e7e15d9a442e8df3bd686f8
Priyanshu <pv254424@gmail.com>
2026-03-29 23:06:49 +0530

    two sum submit

---
+++ src/TaiyariDSA/TwoSum.java
@@ -0,0 +1,17 @@
+package TaiyariDSA;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int  [] arr = {10,2,3,5,7,8};
+        int target = 9;
+        for(int i= 0 ; i < arr.length ; i++){
+            for(int j = i+1 ; j < arr.length ; j++){
+                if(arr[i]+arr[j]==target){
+                    System.out.println(i+","+j);
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
