34c834806d782e6069ebd405160ff5b56db0269f
Priyanshu <pv254424@gmail.com>
2026-03-30 22:26:52 +0530

    reverse array

---
+++ src/TaiyariDSA/ReverseArray.java
@@ -0,0 +1,18 @@
+package TaiyariDSA;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int [] arr = {10,2,4,66,8,64,79,4};
+        int i = 0 , j = arr.length - 1;
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        for(int element : arr) {
+            System.out.print(element + " ");
+        }
+    }
+}
