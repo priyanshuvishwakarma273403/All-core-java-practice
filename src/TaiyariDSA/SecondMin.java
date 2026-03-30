698bd2c718d242126a3c73728f52a20a9f33d9c5
Priyanshu <pv254424@gmail.com>
2026-03-30 22:35:25 +0530

    second min and min

---
+++ src/TaiyariDSA/SecondMin.java
@@ -0,0 +1,22 @@
+package TaiyariDSA;
+
+public class SecondMin {
+    public static void main(String[] args) {
+        int [] arr = {1,3,5,7,3,5,79,64,5,76,5,47};
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        int smin = Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<smin && arr[i] != min){
+                smin=arr[i];
+            }
+        }
+
+        System.out.println(min);
+        System.out.println(smin);
+    }
+}
