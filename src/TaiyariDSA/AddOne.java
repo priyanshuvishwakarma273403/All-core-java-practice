08020f8309e1094b551d8949c7ab633abad08043
Priyanshu <pv254424@gmail.com>
2026-03-31 23:52:20 +0530

    add one solved

---
+++ src/TaiyariDSA/AddOne.java
@@ -0,0 +1,33 @@
+package TaiyariDSA;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Vector;
+
+public class AddOne {
+    public static void main(String[] args) {
+        int [] arr = {9, 9, 9,9,9};
+        System.out.println(addOne(arr));
+
+    }
+
+    static Vector<Integer> addOne(int[] arr) {
+        Vector<Integer> ans =  new Vector<>();
+        int n = arr.length;
+        int carry = 1;
+        for(int i = n-1; i>=0; i--){
+            if(arr[i]+carry <=9){
+                ans.add(arr[i]+carry);
+                carry =0;
+            } else{
+                ans.add(0);
+                carry = 1;
+            }
+        }
+        if(carry ==1){
+            ans.add(1);
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
