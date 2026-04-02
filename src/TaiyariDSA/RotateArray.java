803472ec46038f8bfa6242bcd4c9ca25bb63264d
Priyanshu <pv254424@gmail.com>
2026-04-02 13:31:21 +0530

    rotate the array solved

--- src/TaiyariDSA/RotateArray.java
+++ src/TaiyariDSA/RotateArray.java
@@ -1,7 +1,27 @@
 package TaiyariDSA;
 
+// Output: [3, 4, 5, 1, 2]
 public class RotateArray {
     public static void main(String[] args) {
+       int[] arr = {1, 2, 3, 4, 5};
+       int n = arr.length;
+       int d = 2;
+       d %= n;
+       reverse(arr,0,d-1);
+       reverse(arr, d, n-1);
+       reverse(arr, 0, n-1);
+       for(int ele : arr){
+           System.out.print(ele+" ");
+       }
+    }
+    static void reverse(int[] arr, int i, int j){
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
 
     }
 }
