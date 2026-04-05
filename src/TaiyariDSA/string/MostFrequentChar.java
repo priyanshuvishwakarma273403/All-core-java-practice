c2c660c15967f17c74aef16f043335ed9bf6e3eb
Priyanshu <pv254424@gmail.com>
2026-04-05 23:05:25 +0530

    third approad solved

--- src/TaiyariDSA/string/MostFrequentChar.java
+++ src/TaiyariDSA/string/MostFrequentChar.java
@@ -30,30 +30,53 @@ public class MostFrequentChar {
 //        System.out.println(ans);
 
 
+//        int n = s.length();
+//        int maxFreq = -1;
+//        char ans = s.charAt(0);
+//        char [] arr = s.toCharArray();
+//        Arrays.sort(arr);
+//        int i = 0, j =0;
+//        while(j< n){
+//
+//            if(arr[i] == arr[j]){
+//                j++;
+//            }else{
+//                int freq = j-i;
+//                if(freq > maxFreq){
+//                    maxFreq = freq;
+//                    ans = arr[i];
+//                }
+//                 i = j;
+//            }
+//        }
+//
+//        int freq = j-i;
+//        if(freq > maxFreq){
+//            maxFreq = freq;
+//            ans = arr[i];
+//        }
+//        System.out.println(ans);
+
+
+
+
         int n = s.length();
         int maxFreq = -1;
         char ans = s.charAt(0);
-        char [] arr = s.toCharArray();
-        Arrays.sort(arr);
-        int i = 0, j =0;
-        while(j< n){
-
-            if(arr[i] == arr[j]){
-                j++;
-            }else{
-                int freq = j-i;
-                if(freq > maxFreq){
-                    maxFreq = freq;
-                    ans = arr[i];
+        for(int i = 0; i < n; i++){
+            int freq = 1;
+            char ch = s.charAt(i);
+            for(int j = i+1; j < n; j++){
+                if(s.charAt(j) == ch){
+                    freq++;
                 }
-                 i = j;
             }
-        }
-
-        int freq = j-i;
-        if(freq > maxFreq){
-            maxFreq = freq;
-            ans = arr[i];
+            if(freq > maxFreq){
+                maxFreq = freq;
+                ans = ch;
+            } else if (freq == maxFreq && ch<ans) {
+                ans = ch;
+            }
         }
         System.out.println(ans);
 
