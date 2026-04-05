25d8c4f8beea4f33b9c59b0a98bf75538ba10b68
Priyanshu <pv254424@gmail.com>
2026-04-05 22:43:44 +0530

    anagrams solved

---
+++ src/TaiyariDSA/string/AnagramsOfString.java
@@ -0,0 +1,25 @@
+package TaiyariDSA.string;
+
+import java.util.Arrays;
+
+public class AnagramsOfString {
+    public static void main(String[] args) {
+        String s1 = "Shiva";
+        String s2 = "Rohit";
+        if(s1.length()!=s2.length()){
+            return;
+        }
+        char [] arr1 = s1.toCharArray();
+        char [] arr2 = s2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        for(int i=0;i<arr1.length;i++){
+            if(arr1[i]!=arr2[i]){
+                System.out.println("This are not anagrams");
+                return;
+            }
+        }
+        System.out.println("This String is Anagram");
+    }
+}
