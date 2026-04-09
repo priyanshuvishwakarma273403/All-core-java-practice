075efd42b496b8f1fe090f562f0a47813741ea8d
Priyanshu <pv254424@gmail.com>
2026-04-10 00:22:07 +0530

    palindrome String

--- src/StringLatest/StringBuilderPractice.java
+++ src/StringLatest/StringBuilderPractice.java
@@ -1,5 +1,7 @@
 package StringLatest;
 
+import java.util.StringJoiner;
+
 public class StringBuilderPractice {
     public static void main(String[] args) {
         System.out.println("\n--- StringBuffer (Thread-Safe)---");
@@ -13,5 +15,40 @@ public class StringBuilderPractice {
                 RULE: Always prefer StringBuilder unless multiple threads
                 access the same builder (which is rare).
                 """);
+
+        System.out.println("--- Practical Patterns---");
+        String[] data =  {"Alice", "30", "Engineer"};
+        StringBuilder csv = new StringBuilder();
+        for(int i = 0; i< data.length; i++){
+            if(i > 0) csv.append(',');
+            csv.append(data[i]);
+        }
+        System.out.println("CSV: " + csv);
+
+        // Building with StringJoiner (cleaner)
+        var joiner = new StringJoiner(",");
+        for(String str :  data) joiner.add(str);
+        System.out.println("Joiner: " + joiner);
+
+        // Building HTML/XML
+        String html = new StringBuilder()
+                .append(" <html>\n")
+                .append(" <body>\n")
+                .append(" <h1>Hello</h1>\n")
+                .append(" </body>\n")
+                .append(" </html>")
+                .toString();
+        System.out.println("HTML: " + html);
+
+        // Reverse a string
+        String reversed = new StringBuilder("Hello World").reverse().toString();
+        System.out.println("Reversed: " + reversed);
+
+        // Check palindrome
+        String word = "racecar";
+        boolean isPalindrome = word.equals(new StringBuilder(word).reverse().toString());
+        System.out.println("isPalindrome: " + isPalindrome);
+
+
     }
 }
---
+++ src/StringLatest/StringFormattingGuide.java
@@ -0,0 +1,4 @@
+package StringLatest;
+
+public class StringFormattingGuide {
+}
