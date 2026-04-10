16e8cb9e08419f21a6aa0d7d240f1643aa3004b4
Priyanshu <pv254424@gmail.com>
2026-04-11 00:13:33 +0530

    string template started

---
+++ src/StringLatest/StringTemplatesDemo.java
@@ -0,0 +1,14 @@
+package StringLatest;
+
+public class StringTemplatesDemo {
+    public static void main(String[] args) {
+        System.out.println("═══ String Templates (Java 21 Preview) ═══\n");
+        System.out.println("--- Evolution of String Composition---");
+
+        String name = "Alice";
+        int age = 19;
+        double score = 95.5;
+        String v1 = "Name: "+ name + "Age: " + age + "Score: " + score;
+        System.out.println("Concat: "+v1);
+    }
+}
