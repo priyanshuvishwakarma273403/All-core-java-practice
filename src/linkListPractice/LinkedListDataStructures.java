c8bcbb2c80f0a93331d7a92d6344249abe0578d9
Priyanshu <pv254424@gmail.com>
2026-04-10 23:38:10 +0530

    display all the data

--- src/linkListPractice/LinkedListDataStructures.java
+++ src/linkListPractice/LinkedListDataStructures.java
@@ -13,11 +13,69 @@ class LinkedList{
     Node tail;
     int size;
 
+    int search(int val){
+        if(head == null) return -1;
+        Node temp = head;
+        int idx = 0;
+        while (temp!=null){
+            if(temp.val==val){
+                return idx;
+            }
+            temp = temp.next;
+            idx++;
+        }
+        return -1;
+    }
+
+    void display(){
+        if(head == null) return;
+        Node temp = head;
+        while(temp!=null){
+            System.out.println(temp.val+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    int get(int idx){
+        Node temp = head;
+        for(int i=0;i<idx;i++){
+            temp = temp.next;
+        }
+        return temp.val;
+    }
+
+     void addAtTail(int val) {
+        if(tail == null){
+            addAtTail(val);
+            return;
+        }
+        Node temp = new Node(val);
+        tail.next = temp;
+        tail = temp;
+        size++;
+    }
+
+     void addAtHead(int val) {
+        Node temp = new Node(val);
+        if(head == null){
+            head = tail = temp;
+        }else{
+            temp.next = head;
+            head = temp;
+        }
+        size++;
+    }
 }
 
 public class LinkedListDataStructures {
     public static void main(String[] args) {
-
-
+        LinkedList l1 = new LinkedList();
+        l1.addAtTail(10);
+        l1.addAtTail(20);
+        l1.addAtTail(30);
+        l1.addAtTail(40); l1.display();
+        l1.addAtHead(50);
+        l1.addAtHead(60); l1.display();
     }
 }
