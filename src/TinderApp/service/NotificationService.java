aaa0be78eb9d3ce52a7297acf3324562ea52141e
Priyanshu <pv254424@gmail.com>
2026-04-16 23:02:07 +0530

    NotificationService

---
+++ src/TinderApp/service/NotificationService.java
@@ -0,0 +1,42 @@
+package TinderApp.service;
+
+import TinderApp.repository.NotificationObserver;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NotificationService {
+    private Map<String , NotificationObserver> observers;
+    private static NotificationService instance;
+    public NotificationService() {
+        observers = new HashMap<>();
+    }
+
+    public static NotificationService getInstance(){
+        if(instance == null){
+            instance = new NotificationService();
+        }
+        return instance;
+    }
+
+    public void registerObserver(String userId, NotificationObserver observer){
+        observers.put(userId, observer);
+    }
+
+    public void removeObserver(String userId){
+        observers.remove(userId);
+    }
+
+    public void removeObserver(String userId, String message){
+        if(observers.containsKey(userId)){
+            observers.get(userId).update(message);
+        }
+    }
+
+    public void notifyAll(String message){
+        for(Map.Entry<String, NotificationObserver> pair : observers.entrySet()){
+            pair.getValue().update(message);
+        }
+    }
+
+}
