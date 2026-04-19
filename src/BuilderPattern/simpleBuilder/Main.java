4a72a093008218bf9fb06a7afaa76438a1fc16c1
Priyanshu <pv254424@gmail.com>
2026-04-19 22:26:26 +0530

    HttpRequest main class updated

--- src/BuilderPattern/simpleBuilder/Main.java
+++ src/BuilderPattern/simpleBuilder/Main.java
@@ -2,6 +2,16 @@ package BuilderPattern.simpleBuilder;
 
 public class Main {
     public static void main(String[] args) {
+        HttpRequest request = new HttpRequest.HttpRequestBuilder()
+                .withUrl("https://api.example2.com")
+                .withMethod("POST")
+                .withHeader("Content-Type", "application/json")
+                .withHeader("Accept", "application/json")
+                .withQueryParams("key", "12345")
+                .withBody("{\"name\": \"Priyanshu\"}")
+                .withTimeout(60)
+                .build();
 
+        request.execute();
     }
 }
