765886b42e089f2468eaaaa45a2f2b557bdc2819
Priyanshu <pv254424@gmail.com>
2026-04-19 22:24:37 +0530

    HttpRequest updated

--- src/TaiyariDSA/hashMapPractice/CountPairsInDivisibleK.java
+++ src/TaiyariDSA/hashMapPractice/CountPairsInDivisibleK.java
@@ -1,4 +1,35 @@
 package TaiyariDSA.hashMapPractice;
 
+import java.util.HashMap;
+
 public class CountPairsInDivisibleK {
+    public static void main(String[] args) {
+        int [] arr = {2, 2, 1, 7, 5, 3};
+        int k = 4;
+        int n = arr.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int ele : arr){
+            int x = ele%k;
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        long pairs = 0;
+        long zeroCount = map.getOrDefault(0, 0);
+        pairs += zeroCount * (zeroCount -1) /2;
+        map.remove(0);
+        if(k%2 == 0){
+            long halfCount= map.getOrDefault(k/2, 0);
+            pairs += halfCount * (halfCount -1) /2;
+            map.remove(k/2);
+        }
+        pairs *= 2;
+
+        for(int key : map.keySet()){
+            int rem = k -key;
+            if(map.containsKey(rem))
+                pairs += (map.get(key)) * (map.get(rem));
+        }
+        pairs /= 2;
+        System.out.println(pairs);
+    }
 }
