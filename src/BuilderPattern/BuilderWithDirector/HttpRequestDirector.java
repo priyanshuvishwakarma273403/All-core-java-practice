c83c17cd23779ec250f0c0969d87fd5905b28d1b
Priyanshu <pv254424@gmail.com>
2026-04-19 22:37:07 +0530

    HttpRequestDirector init

--- src/BuilderPattern/BuilderWithDirector/HttpRequestDirector.java
+++ src/BuilderPattern/BuilderWithDirector/HttpRequestDirector.java
@@ -1,5 +1,21 @@
 package BuilderPattern.BuilderWithDirector;
 
 public class HttpRequestDirector {
+    public static HttpRequest createGetRequest(String url) {
+        return new HttpRequest.HttpRequestBuilder()
+                .withUrl(url)
+                .withMethod("GET")
+                .build();
+    }
+
+    public static HttpRequest createJsonPostRequest(String url, String jsonBody) {
+        return new HttpRequest.HttpRequestBuilder()
+                .withUrl(url)
+                .withMethod("POST")
+                .withHeader("Content-Type", "application/json")
+                .withHeader("Accept", "application/json")
+                .withBody(jsonBody)
+                .build();
+    }
 
 }
