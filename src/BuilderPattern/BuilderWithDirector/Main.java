7308c4d4b72b64b06fe7d1cd925083979365e323
Priyanshu <pv254424@gmail.com>
2026-04-19 22:38:00 +0530

    HttpRequestDirector comp

--- src/BuilderPattern/BuilderWithDirector/Main.java
+++ src/BuilderPattern/BuilderWithDirector/Main.java
@@ -1,5 +1,29 @@
 package BuilderPattern.BuilderWithDirector;
 
 public class Main {
+    public static void main(String[] args) {
+        HttpRequest normalRequest = new HttpRequest.HttpRequestBuilder()
+                .withUrl("https://api.example.com")
+                .withMethod("POST")
+                .withHeader("Content-Type", "application/json")
+                .withHeader("Accept", "application/json")
+                .withQueryParams("key", "12345")
+                .withBody("{\"name\": \"Aditya\"}")
+                .withTimeout(60)
+                .build();
 
+        normalRequest.execute(); // Guaranteed to be in a consistent state
+
+        System.out.println("\n----------------------------\n");
+
+        HttpRequest getRequest = HttpRequestDirector.createGetRequest("https://api.example.com/users");
+        getRequest.execute();
+
+        System.out.println("\n----------------------------\n");
+
+        HttpRequest postRequest = HttpRequestDirector.createJsonPostRequest(
+                "https://api.example.com/users",
+                "{\"name\": \"Aditya\", \"email\": \"aditya@example.com\"}");
+        postRequest.execute();
+    }
 }
