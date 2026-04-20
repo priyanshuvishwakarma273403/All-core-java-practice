86dfc310cf8857beb529470fd3e80522a1dd6dd8
Priyanshu <pv254424@gmail.com>
2026-04-20 22:20:30 +0530

    BinaryTreeInorderIterator init

--- src/LldSeries/IteratorPattern.java
+++ src/LldSeries/IteratorPattern.java
@@ -1,6 +1,8 @@
 package LldSeries;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 interface Iterator<T>{
@@ -87,6 +89,37 @@ class LinkedListIterator implements Iterator<Integer>{
         int val = current.data;
         current = current.next;
         return val;
+    }
+}
+
+class BinaryTreeInorderIterator implements Iterator<Integer>{
+
+    private Deque<BinaryTree> stk = new ArrayDeque<>();
+
+    private void pushLefts(BinaryTree node){
+        while (node != null){
+            stk.push(node);
+            node = node.left;
+        }
+    }
+
+    public BinaryTreeInorderIterator(BinaryTree root){
+        pushLefts(root);
+    }
+
+    @Override
+    public boolean hasNext() {
+       return !stk.isEmpty();
+    }
+
+    @Override
+    public Integer next() {
+       BinaryTree node = stk.pop();
+       int val = node.data;
+       if(node.right != null){
+           pushLefts(node.right);
+       }
+       return val;
     }
 }
 
