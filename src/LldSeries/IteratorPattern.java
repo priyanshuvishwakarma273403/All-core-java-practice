1efde6a3233efbe6a2b09288a962e826e1d232a9
Priyanshu <pv254424@gmail.com>
2026-04-20 22:10:36 +0530

    BinaryTreeInorderIterator compl

--- src/LldSeries/IteratorPattern.java
+++ src/LldSeries/IteratorPattern.java
@@ -25,6 +25,23 @@ class LinkedList implements Iterable<Integer>{
     }
 }
 
+class BinaryTree implements Iterable<Integer>{
+
+    public int data;
+    public BinaryTree left;
+    public BinaryTree right;
+
+    public BinaryTree(int value){
+        data = value;
+        left = null;
+        right = null;
+    }
+
+    @Override
+    public Iterator<Integer> getIterator() {
+        return new BinaryTreeInorderIterator(this);
+    }
+}
 
 
 public class IteratorPattern {
