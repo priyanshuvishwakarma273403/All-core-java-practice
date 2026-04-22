e6e1bdd884eb948a46507f59b477dbd1603d21cf
Priyanshu <pv254424@gmail.com>
2026-04-22 21:56:02 +0530

    without fly waight compl

--- src/lldFlightWeight/WithoutFlyWeight.java
+++ src/lldFlightWeight/WithoutFlyWeight.java
@@ -82,12 +82,25 @@ class SpaceGame{
     public int getAsteroidCount() {
         return asteroids.size();
     }
-
 }
-
 
 public class WithoutFlyWeight {
     public static void main(String[] args) {
+        final int ASTEROID_COUNT = 1_000_000;
+        System.out.println("\n TESTING WITHOUT FLYWEIGHT PATTERN");
+        SpaceGame game = new SpaceGame();
+
+        game.spawnAsteroids(ASTEROID_COUNT);
+
+        game.renderAll();
+
+        long totalMemory = game.calculateMemoryUsage();
+
+        System.out.println("\n=== MEMORY USAGE ===");
+        System.out.println("Total asteroids: " + ASTEROID_COUNT);
+        System.out.println("Memory per asteroid: " + Asteroid.getMemoryUsage() + " bytes");
+        System.out.println("Total memory used: " + totalMemory + " bytes");
+        System.out.println("Memory in MB: " + totalMemory / (1024.0 * 1024.0) + " MB");
 
     }
 }
