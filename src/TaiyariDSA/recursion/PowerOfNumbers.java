3e20bb614da16612fb38dc5d41872effed09ceb9
Priyanshu <pv254424@gmail.com>
2026-04-22 21:33:54 +0530

    power of a numbers solved

---
+++ src/TaiyariDSA/recursion/PowerOfNumbers.java
@@ -0,0 +1,21 @@
+package TaiyariDSA.recursion;
+
+public class PowerOfNumbers {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(reverseExponentiation(n));
+    }
+
+    private static int reverseExponentiation(int n) {
+        if(n == 10) return 10;
+        return pow(n,n);
+
+    }
+
+    private static int pow(int a, int b) {
+        if(b == 0) return 1;
+        int call = pow(a, b/2);
+        if(b%2 == 0) return call * call;
+       else return call*call*a;
+    }
+}
