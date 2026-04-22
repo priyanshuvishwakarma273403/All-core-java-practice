4c6fb86f26d4a86317431a204dea8f722b5ea8fc
Priyanshu <pv254424@gmail.com>
2026-04-22 22:03:14 +0530

    AsteroidFactory init

--- src/lldFlightWeight/WithFlightWeight.java
+++ src/lldFlightWeight/WithFlightWeight.java
@@ -1,5 +1,8 @@
 package lldFlightWeight;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class AsteroidFlyweight{
     // Intrinsic properties (shared among asteroids of same type)
     private int length;
@@ -30,6 +33,32 @@ class AsteroidFlyweight{
         return Integer.BYTES * 3 + 40 * 3;
     }
 }
+
+class AsteroidFactory{
+    public static Map<String, AsteroidFlyweight> flyweights = new HashMap<>();
+
+    public static AsteroidFlyweight getAsteroid(int length, int width, int weight, String color, String texture, String material) {
+        String key = length + "_" + width + "_" + weight + "_" + color + "_" + texture + "_" + material;
+
+        if(!flyweights.containsKey(key)){
+            flyweights.put(key, new AsteroidFlyweight(length, width, weight, color, texture, material));
+        }
+        return flyweights.get(key);
+    }
+
+    public static int getFlyweightCount(){
+        return flyweights.size();
+    }
+
+    public static long getTotalFlyweightMemory() {
+        return flyweights.size() * AsteroidFlyweight.getMemoryUsage();
+    }
+
+    public static void cleanup() {
+        flyweights.clear();
+    }
+}
+
 
 public class WithFlightWeight {
     public static void main(String[] args) {
