b59320bb7e9d8e0081a4d29ef342f67fb11ad07d
Priyanshu <pv254424@gmail.com>
2026-04-22 21:58:17 +0530

    with fly class init

--- src/lldFlightWeight/WithFlightWeight.java
+++ src/lldFlightWeight/WithFlightWeight.java
@@ -1,7 +1,35 @@
 package lldFlightWeight;
 
+class AsteroidFlyweight{
+    // Intrinsic properties (shared among asteroids of same type)
+    private int length;
+    private int width;
+    private int weight;
+    private String color;
+    private String texture;
+    private String material;
 
+    public AsteroidFlyweight(int l, int w, int wt, String col, String tex, String mat) {
+        this.length = l;
+        this.width = w;
+        this.weight = wt;
+        this.color = col;
+        this.texture = tex;
+        this.material = mat;
+    }
 
+    public void render(int posX, int posY, int velocityX, int velocityY) {
+        System.out.println("Rendering " + color + ", " + texture + ", " + material
+                + " asteroid at (" + posX + "," + posY
+                + ") Size: " + length + "x" + width
+                + " Velocity: (" + velocityX + ", "
+                + velocityY + ")");
+    }
+
+    public static long getMemoryUsage() {
+        return Integer.BYTES * 3 + 40 * 3;
+    }
+}
 
 public class WithFlightWeight {
     public static void main(String[] args) {
