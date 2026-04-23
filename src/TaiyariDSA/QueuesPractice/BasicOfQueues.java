75b79959efba9a092ab4c7877cf348f5dd1bb007
Priyanshu <pv254424@gmail.com>
2026-04-23 15:54:24 +0530

    basic queue

--- src/TaiyariDSA/QueuesPractice/BasicOfQueues.java
+++ src/TaiyariDSA/QueuesPractice/BasicOfQueues.java
@@ -1,5 +1,15 @@
 package TaiyariDSA.QueuesPractice;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BasicOfQueues {
-    
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(10); q.add(20); q.add(30); q.add(40);
+        System.out.println(q + " "  +q.peek()); // nakli printing
+        q.remove();
+        System.out.println(q + " "  +q.size());
+        System.out.println(q.remove()+ " "  +q);
+    }
 }
