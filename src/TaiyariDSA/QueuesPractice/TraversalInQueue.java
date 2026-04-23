4045783db70b23ced76156d7b39724e6d4c42a59
Priyanshu <pv254424@gmail.com>
2026-04-23 16:53:27 +0530

    traversal in queue solved

---
+++ src/TaiyariDSA/QueuesPractice/TraversalInQueue.java
@@ -0,0 +1,51 @@
+package TaiyariDSA.QueuesPractice;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TraversalInQueue {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(10); q.add(20); q.add(30); q.add(40); q.add(50);
+        display(q);
+        addAtIndex(q,2,60);
+        display(q);
+
+    }
+
+    private static void addAtIndex(Queue<Integer> q, int idx, int val) {
+        if(idx<0 || idx > q.size()){
+            System.out.println("Invalid index !");
+            return;
+        }
+
+        int n = q.size();
+        if(idx<n){
+            for(int i= 1; i<= idx; i++){
+                q.add(q.remove());
+            }
+        }
+        q.add(val);
+        for(int i = 1; i<= n-idx; i++){
+            q.add(q.remove());
+        }
+    }
+
+    private static void display(Queue<Integer> q) {
+            int n =  q.size();
+            for(int i = 1; i<= n; i++){
+                System.out.print(q.peek()+" ");
+                q.add(q.remove());
+            }
+        System.out.println();
+    }
+
+    private static int peek(Queue<Integer> q, int idx){
+        return -1;
+    }
+
+    private static int remove(Queue<Integer> q, int idx){
+        return -1;
+    }
+
+}
