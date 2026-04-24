b39df51f4066b33ed6ee87c1c63a84625ad622af
Priyanshu <pv254424@gmail.com>
2026-04-24 15:46:53 +0530

    peak element solved

---
+++ src/TaiyariDSA/binarySearch/PeakElement1.java
@@ -0,0 +1,23 @@
+package TaiyariDSA.binarySearch;
+
+
+public class PeakElement1 {
+    public static void main(String[] args) {
+        int [] nums = {1,40,100,90, 4};
+        System.out.println(findPeak(nums));
+    }
+
+    private static int findPeak(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while(low < high){
+            int mid =   (low +high)/2;
+            if(nums[mid]  > nums[mid+1]){
+                high = mid;
+            } else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+}
