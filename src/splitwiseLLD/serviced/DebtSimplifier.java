d40a3bcc4082bdfe143b56e464b733e2084a4b64
Priyanshu <pv254424@gmail.com>
2026-04-27 21:00:36 +0530

    simplify dept use greedy algorith for better optimization

--- src/splitwiseLLD/serviced/DebtSimplifier.java
+++ src/splitwiseLLD/serviced/DebtSimplifier.java
@@ -1,7 +1,6 @@
 package splitwiseLLD.serviced;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class DebtSimplifier {
     public static Map<String, Map<String, Double>> simlifyDebts(
@@ -23,6 +22,56 @@ public class DebtSimplifier {
                 }
             }
         }
+
+        List<AbstractMap.SimpleEntry<String, Double>> creditors = new ArrayList<>();
+        List<AbstractMap.SimpleEntry<String, Double>> debtors = new ArrayList<>();
+        for(Map.Entry<String, Double> net :  netAmounts.entrySet()){
+            if(net.getValue() > 0.01){
+                creditors.add(new AbstractMap.SimpleEntry<>(net.getKey(), net.getValue()));
+            } else if(net.getValue() < -0.01){
+                debtors.add(new AbstractMap.SimpleEntry<>(net.getKey(), -net.getValue()));
+            }
+        }
+
+        // Sort for better optimization (largest amounts first)
+        creditors.sort((a, b) -> Double.compare(b.getValue(), a.getValue()));
+        debtors.sort((a, b) -> Double.compare(b.getValue(), a.getValue()));
+
+        // Create new simplified balance map
+        Map<String, Map<String, Double>> simplifiedBalances = new HashMap<>();
+        for (Map.Entry<String, Map<String, Double>> userBalance : groupBalances.entrySet()) {
+            simplifiedBalances.put(userBalance.getKey(), new HashMap<>());
+        }
+        // Use greedy algorithm to minimize transactions
+        int i = 0, j = 0;
+        while (i < creditors.size() && j < debtors.size()) {
+            String creditorId = creditors.get(i).getKey();
+            String debtorId = debtors.get(j).getKey();
+            double creditorAmount = creditors.get(i).getValue();
+            double debtorAmount = debtors.get(j).getValue();
+
+            // Find the minimum amount to settle
+            double settleAmount = Math.min(creditorAmount, debtorAmount);
+
+            // Update simplified balances
+            // debtorId owes creditorId the settleAmount
+            simplifiedBalances.get(creditorId).put(debtorId, settleAmount);
+            simplifiedBalances.get(debtorId).put(creditorId, -settleAmount);
+
+            // Update remaining amounts
+            creditors.get(i).setValue(creditors.get(i).getValue() - settleAmount);
+            debtors.get(j).setValue(debtors.get(j).getValue() - settleAmount);
+
+            // Move to next creditor or debtor if current one is settled
+            if (creditors.get(i).getValue() < 0.01) {
+                i++;
+            }
+            if (debtors.get(j).getValue() < 0.01) {
+                j++;
+            }
+        }
+
+        return simplifiedBalances;
 
     }
 
