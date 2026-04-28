bbc5499fa1744ab2e5dee07d7437fecad5bbdc68
Priyanshu <pv254424@gmail.com>
2026-04-28 18:13:46 +0530

    dfs level inorder and post order method updated fix all

--- src/TaiyariDSA/binarySearch/dfs_level.java
+++ src/TaiyariDSA/binarySearch/dfs_level.java
@@ -26,8 +26,32 @@ public class dfs_level {
         a.left = b; a.right = c;
         b.left = d; b.right = e;
         c.left = f; c.right = g;
+        preOrder(a);
+        System.out.println();
+        inOrder(a);
+        System.out.println();
+        postOrder(a);
+    }
 
 
+    private static int levels(Node root){
+        if(root == null) return 0;
+        return 1+ Math.max(levels(root.left), levels(root.right));
+    }
+
+    private static int  size(Node root){
+        if(root == null) return 0;
+        return 1 + size(root.left) + size(root.right);
+    }
+
+    private static int sum(Node root){
+        if(root == null) return 0;
+        return root.val + sum(root.left) + sum(root.right);
+    }
+
+    private static int max(Node root){
+        if(root == null) return Integer.MIN_VALUE;
+        return Math.max(root.val, Math.max(max(root.left), max(root.right)));
     }
 
     private static void preOrder(Node root){
