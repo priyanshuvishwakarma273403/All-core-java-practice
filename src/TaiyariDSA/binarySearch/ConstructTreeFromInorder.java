8721c9f21dec7fd2e7ece7c1db282f7d8ecce3a2
Priyanshu <pv254424@gmail.com>
2026-04-28 18:26:24 +0530

    construct tree from inorder updated new method

--- src/TaiyariDSA/binarySearch/ConstructTreeFromInorder.java
+++ src/TaiyariDSA/binarySearch/ConstructTreeFromInorder.java
@@ -13,7 +13,18 @@ public class ConstructTreeFromInorder {
     public static void main(String[] args) {
         int [] inorder = {3, 1, 4, 0, 5, 2};
         int[] preorder = {0, 1, 3, 4, 2, 5};
+        buildTree(inorder, preorder);
+    }
+
+    public static Node buildTree(int [] inorder, int [] preorder){
+        int n =  inorder.length;
+        return build(0,n-1,n-1, inorder,preorder);
+    }
+
+    private static Node build(int preLo, int preHi, int inLo, int[] inorder, int[] preorder) {
+
 
 
     }
+
 }
