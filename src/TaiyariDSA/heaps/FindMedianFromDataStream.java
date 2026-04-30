cd6d4097ed04d2fbd33a70fe2196df0c7cb0db16
Priyanshu <pv254424@gmail.com>
2026-04-30 23:11:02 +0530

    find median from data stream some method updated finally fix and solved

--- src/TaiyariDSA/heaps/FindMedianFromDataStream.java
+++ src/TaiyariDSA/heaps/FindMedianFromDataStream.java
@@ -20,10 +20,21 @@ class MedianFinder{
         if(maxHeap.size() == minHeap.size() + 2) minHeap.add(maxHeap.remove());
         if(minHeap.size() == maxHeap.size() + 2) maxHeap.add(minHeap.remove());
     }
+
+    public double findMedian(){
+        if(maxHeap.size() == minHeap.size() + 1)  return maxHeap.peek();
+        else if(minHeap.size() == maxHeap.size() + 1)  return minHeap.peek();
+        else return (maxHeap.peek() + minHeap.peek()) / 2.0;
+    }
 }
 
 public class FindMedianFromDataStream {
     public static void main(String[] args) {
+        MedianFinder obj = new MedianFinder();
+        obj.addNum(1);
+        obj.addNum(2);
+        double median = obj.findMedian();
+        System.out.println(median);
 
     }
 }
