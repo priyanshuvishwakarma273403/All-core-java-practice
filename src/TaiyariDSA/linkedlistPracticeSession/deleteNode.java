58b8cf58607e38a027c91ed10d97a2b28aa585b2
Priyanshu <pv254424@gmail.com>
2026-04-30 23:37:43 +0530

    delete Node from linked list build custom removal method

---
+++ src/TaiyariDSA/linkedlistPracticeSession/deleteNode.java
@@ -0,0 +1,28 @@
+package TaiyariDSA.linkedlistPracticeSession;
+
+class ListNode{
+    int val;
+    ListNode next;
+    public ListNode(int val){
+        this.val = val;
+    }
+
+    public void delete(ListNode head){
+        head.val = head.next.val;
+        head.next = head.next.next;
+    }
+}
+
+public class deleteNode {
+    public static void main(String[] args) {
+        ListNode target =  new ListNode(4);
+        target.next = new ListNode(5);
+        target.next.next = new ListNode(1);
+        target.next.next.next = new ListNode(9);
+        target.delete(target.next);
+        while(target != null){
+            System.out.print(target.val + " ");
+            target = target.next;
+        }
+    }
+}
