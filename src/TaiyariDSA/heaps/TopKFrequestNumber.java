f65f63fb543f27da95e5c18e3a6591387f2266e2
Priyanshu <pv254424@gmail.com>
2026-04-30 23:21:46 +0530

    top k frequest array updated pair classes and impl Comparable interface solved all bug and fixes finally

--- src/TaiyariDSA/heaps/TopKFrequestNumber.java
+++ src/TaiyariDSA/heaps/TopKFrequestNumber.java
@@ -1,6 +1,9 @@
 package TaiyariDSA.heaps;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.PriorityQueue;
 
 class Pair implements Comparable<Pair>{
     int ele ;
@@ -21,11 +24,26 @@ public class TopKFrequestNumber {
     public static void main(String[] args) {
         int [] arr = {3, 1, 4, 4, 5, 2, 6, 1};
         int k = 2;
-        topKFreq(arr,k);
+        System.out.println( topKFreq(arr,k));
     }
 
     private static ArrayList<Integer> topKFreq(int[] arr, int k) {
-
-
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int ele : arr){
+            map.put(ele,map.getOrDefault(ele,0)+1);
+        }
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for(int ele : map.keySet()){
+            int freq = map.get(ele);
+            pq.add(new Pair(ele,freq));
+            if(pq.size()>k) pq.poll();
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        while(pq.size()>0){
+            Pair top = pq.poll();
+            ans.add(top.ele);
+        }
+        Collections.reverse(ans);
+        return ans;
     }
 }
