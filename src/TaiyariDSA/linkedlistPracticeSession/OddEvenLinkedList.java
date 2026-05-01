b56303d36ea5aa34fdafafecaee422ef0360a858
Priyanshu <pv254424@gmail.com>
2026-05-01 21:29:25 +0530

    odd and even LL problem solving

---
+++ src/TaiyariDSA/linkedlistPracticeSession/OddEvenLinkedList.java
@@ -0,0 +1,16 @@
+package TaiyariDSA.linkedlistPracticeSession;
+
+public class OddEvenLinkedList {
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
+        head.next.next.next.next.next.next = new Node(7);
+        head.next.next.next.next.next.next.next = new Node(8);
+        head.next.next.next.next.next.next.next.next = new Node(9);
+        head.next.next.next.next.next.next.next.next.next = new Node(10);
+    }
+}
