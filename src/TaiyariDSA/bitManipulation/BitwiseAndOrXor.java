f84dc78e6cad544991d13d5b69c01a3d808855f4
Priyanshu <pv254424@gmail.com>
2026-05-01 21:08:31 +0530

    bit manipulation normal practice

--- src/TaiyariDSA/bitManipulation/BitwiseAndOrXor.java
+++ src/TaiyariDSA/bitManipulation/BitwiseAndOrXor.java
@@ -2,6 +2,13 @@ package TaiyariDSA.bitManipulation;
 
 public class BitwiseAndOrXor {
     public static void main(String[] args) {
+        System.out.println(51&42);
+        System.out.println(51|42);
 
+        int x = 5;
+        System.out.println(~x); // 1s complement
+        System.out.println(-x-1); // 1s complement
+        System.out.println(~x+1); // 2s complement
+        System.out.println(-x); // 2s complement
     }
 }
