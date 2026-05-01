1b6402582e8eea009f83ee179e7d9812b3513928
Priyanshu <pv254424@gmail.com>
2026-05-01 21:39:56 +0530

    odd and even LL problem solving list node solved finally

--- src/TaiyariDSA/linkedlistPracticeSession/OddEvenLinkedList.java
+++ src/TaiyariDSA/linkedlistPracticeSession/OddEvenLinkedList.java
@@ -3,15 +3,42 @@ package TaiyariDSA.linkedlistPracticeSession;
 
 public class OddEvenLinkedList {
     public static void main(String[] args) {
-        Node head = new Node(1);
-        head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = new Node(4);
-        head.next.next.next.next = new Node(5);
-        head.next.next.next.next.next = new Node(6);
-        head.next.next.next.next.next.next = new Node(7);
-        head.next.next.next.next.next.next.next = new Node(8);
-        head.next.next.next.next.next.next.next.next = new Node(9);
-        head.next.next.next.next.next.next.next.next.next = new Node(10);
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        head = oddEvenList(head);
+
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+
+    private static ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+        ListNode oddDummy = new ListNode(-1);
+        ListNode evenDummy = new ListNode(-1);
+        ListNode odd = oddDummy;
+        ListNode even = evenDummy;
+        ListNode curr = head;
+
+        while (curr != null) {
+            odd.next = curr;
+            odd = odd.next;
+            curr = curr.next;
+            if (curr != null) {
+                even.next = curr;
+                even = even.next;
+                curr = curr.next;
+            }
+        }
+
+        even.next = null; // imp
+        odd.next = evenDummy.next;
+        return oddDummy.next;
     }
 }
