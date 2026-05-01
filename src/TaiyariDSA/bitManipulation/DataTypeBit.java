ab0bb6cea64973d2e0f8e89f21c10ae384290cdc
Priyanshu <pv254424@gmail.com>
2026-05-01 21:10:38 +0530

    bit manipulation normal practice second

---
+++ src/TaiyariDSA/bitManipulation/DataTypeBit.java
@@ -0,0 +1,13 @@
+package TaiyariDSA.bitManipulation;
+
+public class DataTypeBit {
+    public static void main(String[] args) {
+        byte x = 127;
+        x+=3;
+        System.out.println(x);
+
+        int y = Integer.MAX_VALUE;
+        y+=10;
+        System.out.println(y);
+    }
+}
