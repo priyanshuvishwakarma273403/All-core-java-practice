04f8de1cb0bd1c2527ed270a5c87d489857d76e1
Priyanshu <pv254424@gmail.com>
2026-05-01 21:51:52 +0530

    palindrome check in LL some method updated solved all properly

--- src/TaiyariDSA/linkedlistPracticeSession/PalindromeInLinkedList.java
+++ src/TaiyariDSA/linkedlistPracticeSession/PalindromeInLinkedList.java
@@ -5,18 +5,44 @@ public class PalindromeInLinkedList {
         Node head = new  Node(1);
         head.next = new Node(2);
         head.next.next = new Node(2);
-        head.next.next.next = new Node(1);
+        head.next.next.next = new Node(3);
 
-        reverse(head);
-        isPalindrome(head);
-
+        System.out.println( isPalindrome(head));
+    }
+    static Node reverse(Node head) {
+        Node curr = head;
+        Node prev = null;
+        Node fwd = null;
+        while (curr != null) {
+            fwd = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = fwd;
+        }
+       return  prev;
     }
 
     private static boolean isPalindrome(Node head) {
+        Node slow =  head;
+        Node fast = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
 
-    }
+        Node head2 = slow.next;
+        slow.next = null;
+         head2 =reverse(head2);
 
-    static void reverse(Node head) {
-
+        Node i = head;
+        Node j = head2;
+        while(j!=null){
+            if(i.data != j.data){
+                return false;
+            }
+            i = i.next;
+            j = j.next;
+        }
+        return true;
     }
 }
