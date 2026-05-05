5a3b845e61b1aa2cf008ce52eb4ad0c4b5c2a157
Priyanshu <pv254424@gmail.com>
2026-05-05 09:41:53 +0530

    two ki power problem solved for practice

---
+++ src/TaiyariDSA/bitManipulation/TwoKiPower.java
@@ -0,0 +1,11 @@
+package TaiyariDSA.bitManipulation;
+
+import java.util.Scanner;
+
+public class TwoKiPower {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(1<<n);
+    }
+}
