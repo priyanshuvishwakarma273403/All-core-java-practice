25f779446c2e056b3f509a71d9b6336aa28b2df3
Priyanshu <pv254424@gmail.com>
2026-05-05 12:33:21 +0530

    sort an array using loops solved

--- src/TaiyariDSA/string/SortAnArray.java
+++ src/TaiyariDSA/string/SortAnArray.java
@@ -5,7 +5,19 @@ import java.util.Arrays;
 public class SortAnArray {
     public static void main(String[] args) {
         int [] arr = {5, 2, 8, 1, 3};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+//        Arrays.sort(arr);  with method
+//        System.out.println(Arrays.toString(arr));
+        for(int i = 0; i < arr.length; i++){
+            for (int j = i+1; j < arr.length; j++){
+                if(arr[i]>arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for(int ele : arr){
+            System.out.print(ele+ " ");
+        }
     }
 }
