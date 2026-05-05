0e29f27bf19dad8d4dc341d51fa2137d9e5acdbe
Priyanshu <pv254424@gmail.com>
2026-05-05 23:33:31 +0530

    remove duplicated using loop problem solved

--- src/TaiyariDSA/array/RemoveDuplicatesInArray.java
+++ src/TaiyariDSA/array/RemoveDuplicatesInArray.java
@@ -5,10 +5,29 @@ import java.util.HashSet;
 public class RemoveDuplicatesInArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 2, 3, 4, 4};
-        HashSet<Integer> set = new HashSet<>();
-        for(int ele : arr){
-            set.add(ele);
+        int k = 0;  // this is using for loop
+        for(int i=0;i<arr.length;i++){
+            boolean flag = false;
+            for(int j = 0; j < k; j++){
+                if(arr[i]==arr[j]){
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag){
+                arr[k]=arr[i];
+                k++;
+            }
         }
-        System.out.println(set);
+
+        for(int i=0;i<k;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+//        HashSet<Integer> set = new HashSet<>();
+//        for(int ele : arr){
+//            set.add(ele);
+//        }
+//        System.out.println(set);
     }
 }
