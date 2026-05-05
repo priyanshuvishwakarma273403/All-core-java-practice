e8e9a2a805d42da0348ace9db4958f8b4e86cfea
Priyanshu <pv254424@gmail.com>
2026-05-05 11:45:38 +0530

    fibonacchi series solved problem

---
+++ src/TaiyariDSA/string/FibonacchiSeries.java
@@ -0,0 +1,13 @@
+package TaiyariDSA.string;
+
+public class FibonacchiSeries {
+    public static void main(String[] args) {
+        int n = 6;
+        System.out.println(fibonacci(n));
+    }
+
+    private static int fibonacci(int n) {
+        if (n == 1 || n == 2) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
