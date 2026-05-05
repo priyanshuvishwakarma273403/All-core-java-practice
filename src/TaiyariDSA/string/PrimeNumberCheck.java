04d08a6f2505b3afc0ded6844e5d53aa5ecec055
Priyanshu <pv254424@gmail.com>
2026-05-05 12:13:34 +0530

    prime number solved

---
+++ src/TaiyariDSA/string/PrimeNumberCheck.java
@@ -0,0 +1,15 @@
+package TaiyariDSA.string;
+
+public class PrimeNumberCheck {
+    public static void main(String[] args) {
+        int num = 10;
+        boolean isPrime = true;
+        for(int i = 2; i<= Math.sqrt(num); i++){
+            if(num%i==0){
+                isPrime = false;
+                break;
+            }
+        }
+        System.out.println(isPrime);
+    }
+}
