8c99614fc0d488b4390ba1dcf06da349c3e6ea17
Priyanshu <pv254424@gmail.com>
2026-05-06 11:08:47 +0530

    check for anagram using loops and method

--- src/TaiyariDSA/string/CheckForAnagram.java
+++ src/TaiyariDSA/string/CheckForAnagram.java
@@ -5,11 +5,29 @@ import java.util.Arrays;
 public class CheckForAnagram {
     public static void main(String[] args) {
         String str1 = "silent";
-        String str2 = "listen";
+        String str2 = "talent";
+
+        if(str1.length()!=str2.length()) {
+            System.out.println("this is not anagram");
+            return;
+        }
         char[] arr1 = str1.toCharArray();
         char[] arr2 = str2.toCharArray();
         Arrays.sort(arr1);
         Arrays.sort(arr2);
-        System.out.println(Arrays.equals(arr1, arr2));
+        for(int i=0;i<arr1.length;i++){
+            if(arr1[i]!=arr2[i]){
+                System.out.println("this is not anagram");
+                return;
+            }
+        }
+        System.out.println("this is anagram");
+
+        // using method
+//        char[] arr1 = str1.toCharArray();
+//        char[] arr2 = str2.toCharArray();
+//        Arrays.sort(arr1);
+//        Arrays.sort(arr2);
+//        System.out.println(Arrays.equals(arr1, arr2));
     }
 }
