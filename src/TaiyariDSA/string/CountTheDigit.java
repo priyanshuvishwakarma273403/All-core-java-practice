7d918d52ee91f609d20d1e350e918f876e83c230
Priyanshu <pv254424@gmail.com>
2026-05-06 11:10:21 +0530

    count the number of digit solved

---
+++ src/TaiyariDSA/string/CountTheDigit.java
@@ -0,0 +1,9 @@
+package TaiyariDSA.string;
+
+public class CountTheDigit {
+    public static void main(String[] args) {
+        int num = 12345;
+        int count = String.valueOf(num).length();
+        System.out.println(count);
+    }
+}
