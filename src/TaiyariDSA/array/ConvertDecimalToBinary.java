a6cb9d88d199a541be8f7e0792b0d92399ecce87
Priyanshu <pv254424@gmail.com>
2026-05-06 11:47:17 +0530

    convert deciaml to binary

---
+++ src/TaiyariDSA/array/ConvertDecimalToBinary.java
@@ -0,0 +1,9 @@
+package TaiyariDSA.array;
+
+public class ConvertDecimalToBinary {
+    public static void main(String[] args) {
+        int num = 1;
+        String binary = Integer.toBinaryString(num);
+        System.out.println(binary);
+    }
+}
