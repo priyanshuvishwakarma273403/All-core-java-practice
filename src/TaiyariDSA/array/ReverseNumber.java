9e43db64f687c43ffd731ad089c8e1fd777fe168
Priyanshu <pv254424@gmail.com>
2026-05-06 10:49:49 +0530

    reverse number using maths formula

---
+++ src/TaiyariDSA/array/ReverseNumber.java
@@ -0,0 +1,13 @@
+package TaiyariDSA.array;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int num = 12345;
+        int reversed = 0;
+        while(num!=0){
+            reversed = reversed*10+num%10;
+            num /= 10;
+        }
+        System.out.println(reversed);
+    }
+}
