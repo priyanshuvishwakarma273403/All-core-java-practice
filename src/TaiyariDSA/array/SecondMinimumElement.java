af784617416fff549746051f3592aa4d3f54db88
Priyanshu <pv254424@gmail.com>
2026-05-06 11:28:00 +0530

    second min solved using loop

---
+++ src/TaiyariDSA/array/SecondMinimumElement.java
@@ -0,0 +1,14 @@
+package TaiyariDSA.array;
+
+public class SecondMinimumElement {
+    public static void main(String[] args) {
+        int[] arr = {12, 35, 1, 10, 34, 1};
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
