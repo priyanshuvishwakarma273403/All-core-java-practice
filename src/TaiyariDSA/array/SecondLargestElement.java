108f828ea954b871bd516ddba49d0011892abbc0
Priyanshu <pv254424@gmail.com>
2026-05-06 11:26:08 +0530

    second larget in array second approaches max and smax both solved

--- src/TaiyariDSA/array/SecondLargestElement.java
+++ src/TaiyariDSA/array/SecondLargestElement.java
@@ -3,20 +3,39 @@ package TaiyariDSA.array;
 public class SecondLargestElement {
     public static void main(String[] args) {
         int[] arr = {12, 35, 1, 10, 34, 1};
+         int max = arr[0];
+         for (int i = 1; i < arr.length; i++) {
+             if(arr[i] > max){
+                 max = arr[i];
+             }
+         }
+        System.out.println(max);
 
-        int first = Integer.MIN_VALUE;
-        int second = Integer.MIN_VALUE;
+         int smax = arr[0];
+         for (int i = 1; i < arr.length; i++) {
+             if(arr[i] > smax && arr[i]!=max){
+                 smax = arr[i];
+             }
+         }
+        System.out.println(smax);
 
-        for (int num : arr) {
-            if (num > first) {
-                second = first; // old largest becomes second
-                first = num;    // new largest
-            }
-            else if (num > second && num != first) {
-                second = num;
-            }
-        }
 
-        System.out.println(second);
+
+
+
+//        int first = Integer.MIN_VALUE;
+//        int second = Integer.MIN_VALUE;
+//
+//        for (int num : arr) {
+//            if (num > first) {
+//                second = first; // old largest becomes second
+//                first = num;    // new largest
+//            }
+//            else if (num > second && num != first) {
+//                second = num;
+//            }
+//        }
+//
+//        System.out.println(second);
     }
 }
