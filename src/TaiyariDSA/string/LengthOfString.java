b0aa598d94f5ad5037f735d1275631101ede7af2
Priyanshu <pv254424@gmail.com>
2026-05-07 10:58:29 +0530

    is blank checking in the string

--- src/TaiyariDSA/string/LengthOfString.java
+++ src/TaiyariDSA/string/LengthOfString.java
@@ -3,6 +3,9 @@ package TaiyariDSA.string;
 public class LengthOfString {
     public static void main(String[] args) {
 
+        String str = "";
+        System.out.println(str.isBlank());
+
 //        length of string find
 //        String s  = "Priyanshu";
 //        System.out.println(s.length());
