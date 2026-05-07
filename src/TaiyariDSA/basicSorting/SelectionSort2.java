66e926fe20d31276a0ae9c5c4f5e91efff861409
Priyanshu <pv254424@gmail.com>
2026-05-07 13:55:21 +0530

    selection sorting solved problem

--- src/TaiyariDSA/basicSorting/SelectionSort2.java
+++ src/TaiyariDSA/basicSorting/SelectionSort2.java
@@ -1,4 +1,22 @@
 package TaiyariDSA.basicSorting;
 
 public class SelectionSort2 {
+    public static void main(String[] args) {
+        int [] arr = {64, 25, 12, 22, 11};
+        int n = arr.length;
+        for(int i = 0; i < n-1; i++){
+            int minIndex = i;
+            for(int j = i+1; j < n; j++){
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+        for(int ele : arr){
+            System.out.print(ele + " ");
+        }
+    }
 }
