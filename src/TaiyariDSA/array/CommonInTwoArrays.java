2729b9f1b0a4f163ad393b6eca3726a6440666e9
Priyanshu <pv254424@gmail.com>
2026-05-07 11:23:54 +0530

    common in two arrays solved

--- src/TaiyariDSA/array/CommonInTwoArrays.java
+++ src/TaiyariDSA/array/CommonInTwoArrays.java
@@ -7,9 +7,11 @@ public class CommonInTwoArrays {
         int[] arr1 = {1, 2, 3, 4};
         int[] arr2 = {3, 4, 5, 6};
         HashSet<Integer> set = new HashSet<>();
+
         for(int num : arr1){
             set.add(num);
         }
+
         for(int num : arr2){
             if(set.contains(num)){
                 System.out.println(num+" ");
