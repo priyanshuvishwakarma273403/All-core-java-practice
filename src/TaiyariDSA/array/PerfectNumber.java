2fa18022fa17ef0b90caf8c520ddceb1aee4bb31
Priyanshu <pv254424@gmail.com>
2026-05-07 10:52:42 +0530

    solve the perfect number using user input

--- src/TaiyariDSA/array/PerfectNumber.java
+++ src/TaiyariDSA/array/PerfectNumber.java
@@ -1,8 +1,13 @@
 package TaiyariDSA.array;
 
+import java.util.Scanner;
+
 public class PerfectNumber {
     public static void main(String[] args) {
-        int nums = 28;
+//        int nums = 28;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter numbers :  ");
+        int nums = sc.nextInt();
         int sum = 0;
         for(int i= 1; i<= nums/2; i++){
            if(nums % i == 0){
