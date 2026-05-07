d8a65886b326de8fee70f145484c27cb9e525aad
Priyanshu <pv254424@gmail.com>
2026-05-07 11:57:27 +0530

    string contains solved the problem

--- src/TaiyariDSA/array/MergeTwoArrays.java
+++ src/TaiyariDSA/array/MergeTwoArrays.java
@@ -4,13 +4,14 @@ public class MergeTwoArrays {
     public static void main(String[] args) {
         int [] arr1 = {1,3,5};
         int [] arr2 = {2,4,6};
-        int [] merged = new  int[arr1.length+arr2.length];
+        int [] merged = new int[arr1.length+arr2.length];
         int index = 0;
 
         for(int i = 0; i < arr1.length; i++){
             merged[index] = arr1[i];
             index++;
         }
+
         for(int i = 0; i < arr2.length; i++){
             merged[index] = arr2[i];
             index++;
---
+++ src/TaiyariDSA/string/StringContains.java
@@ -0,0 +1,9 @@
+package TaiyariDSA.string;
+
+public class StringContains {
+    public static void main(String[] args) {
+        String str1 = "automation";
+        String str2 = "automation";
+        System.out.println(str1.contains(str2));
+    }
+}
