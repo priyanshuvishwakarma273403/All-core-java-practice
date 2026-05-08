4f7d8ba035c0c3022a2cbc120c54ebbd18598c2d
Priyanshu <pv254424@gmail.com>
2026-05-08 14:44:07 +0530

    second largest element found

---
+++ src/TcsInterview/FindSecontLargest.java
@@ -0,0 +1,20 @@
+package TcsInterview;
+
+import java.util.Scanner;
+
+public class FindSecontLargest {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE;
+        for(int ele : arr){
+            if(ele > first){
+               second = first;
+               first = ele;
+            }else if(ele > second && ele != first){
+                second = ele;
+            }
+        }
+        System.out.println(second);
+        System.out.println(first);
+    }
+}
