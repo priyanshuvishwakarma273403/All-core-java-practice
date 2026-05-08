b14a547065dda0ed80e823b9ddea1378f69ac846
Priyanshu <pv254424@gmail.com>
2026-05-08 13:58:56 +0530

    check leap year solved problem

---
+++ src/TcsInterview/CheckLeapYear.java
@@ -0,0 +1,13 @@
+package TcsInterview;
+
+import java.util.Scanner;
+
+public class CheckLeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the year : ");
+        int year = sc.nextInt();
+        if((year % 400==0 || year % 100 != 0) && (year % 4 == 0)) System.out.println("Leap Year");
+        else System.out.println("Not Leap Year");
+    }
+}
