e1d236afbcd91bcd8bdbd57977304df4e24869ec
Priyanshu <pv254424@gmail.com>
2026-05-08 22:21:37 +0530

    Hourglass  pattern  pattern solve

---
+++ src/TcsInterview/PatternPrinting/HourglassPattern.java
@@ -0,0 +1,23 @@
+package TcsInterview.PatternPrinting;
+
+public class HourglassPattern {
+    public static void main(String[] args) {
+        int n = 4;
+
+        for(int i = n; i >=1; i--) {
+            for(int j = 1; j <= n-i; j++) System.out.print(" ");
+
+            for(int j = 1; j <= 2*i-1; j++) System.out.print("*");
+
+            System.out.println();
+        }
+
+        for(int i = 2; i <= n; i++) {
+            for(int j = 1; j <= n-i; j++) System.out.print(" ");
+
+            for(int j = 1; j <= 2*i-1; j++) System.out.print("*");
+
+            System.out.println();
+        }
+    }
+}
