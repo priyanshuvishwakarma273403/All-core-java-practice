cc80a925f9cd56401e6153f6efa85fd60bb75954
Priyanshu <pv254424@gmail.com>
2026-05-08 19:17:07 +0530

    fibonacci series solved

---
+++ src/TcsInterview/Fibo2.java
@@ -0,0 +1,13 @@
+package TcsInterview;
+
+public class Fibo2 {
+    public static void main(String[] args) {
+        int a = 0 , b = 1;
+        for(int i = 1; i<=6 ; i++){
+            System.out.print(a+" ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
