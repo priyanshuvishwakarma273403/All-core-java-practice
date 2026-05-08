358d242a152fed7d239f7ef30c561566b4c70934
Priyanshu <pv254424@gmail.com>
2026-05-08 22:11:56 +0530

    Reversed Number triangle pattern  pattern solved

---
+++ src/TcsInterview/PatternPrinting/ReverseNumberTriangle.java
@@ -0,0 +1,14 @@
+package TcsInterview.PatternPrinting;
+
+public class ReverseNumberTriangle {
+    public static void main(String[] args) {
+        int n = 4;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
