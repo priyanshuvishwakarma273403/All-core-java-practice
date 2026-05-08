8a2f93e38bd89f6b4f000acc53e404e9bb85eb4f
Priyanshu <pv254424@gmail.com>
2026-05-08 21:53:31 +0530

    Floyd Triangle patten printing

---
+++ src/TcsInterview/PatternPrinting/FloydTriangle.java
@@ -0,0 +1,15 @@
+package TcsInterview.PatternPrinting;
+
+public class FloydTriangle {
+    public static void main(String[] args) {
+        int n = 4;
+        int num = 1;
+        for(int i = 0 ; i <= n ; i++){
+            for(int j = 0 ; j <= i ; j++){
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
