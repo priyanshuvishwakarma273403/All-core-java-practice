4d0b76c758e06a4c26d4febcf563f3deeeea360e
Priyanshu <pv254424@gmail.com>
2026-05-08 09:16:30 +0530

    fibonacci series solved using recursion

--- src/TcsInterview/FibonacciSeries.java
+++ src/TcsInterview/FibonacciSeries.java
@@ -7,12 +7,19 @@ public class FibonacciSeries {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number: ");
         int n = sc.nextInt();
-        int a = 0, b = 1;
-        for(int i=1;i<=n;i++){
-            System.out.println(a + " ");
-            int c = a + b;
-            a = b;
-            b = c;
-        }
+        System.out.println(fibo(n));
+
+//        int a = 0, b = 1;
+//        for(int i=1;i<=n;i++){
+//            System.out.println(a + " ");
+//            int c = a + b;
+//            a = b;
+//            b = c;
+//        }
+    }
+
+    private static int fibo(int n) {
+        if(n <= 1) return n;
+        return fibo(n-1)+fibo(n-2);
     }
 }
