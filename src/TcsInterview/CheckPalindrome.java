97d723777d6d9f466cca3069f4b27992295dc2dc
Priyanshu <pv254424@gmail.com>
2026-05-08 09:33:33 +0530

    palodromic a number solved

---
+++ src/TcsInterview/CheckPalindrome.java
@@ -0,0 +1,17 @@
+package TcsInterview;
+
+import java.util.Scanner;
+
+public class CheckPalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int temp = n , rev = 0;
+        while(n != 0){
+            rev = rev * 10 + n % 10;
+            n = n / 10;
+        }
+        System.out.println(temp == rev ? "Palindrome" : "Not Palindrome");
+    }
+}
