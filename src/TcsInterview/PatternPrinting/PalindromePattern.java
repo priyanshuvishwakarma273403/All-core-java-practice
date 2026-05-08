b6f7261ff16d1752f1a5b95043a1d5dfd782d41e
Priyanshu <pv254424@gmail.com>
2026-05-08 22:15:38 +0530

    Palindrome pattern  pattern solve

---
+++ src/TcsInterview/PatternPrinting/PalindromePattern.java
@@ -0,0 +1,20 @@
+package TcsInterview.PatternPrinting;
+
+public class PalindromePattern {
+    public static void main(String[] args) {
+        int n = 4;
+
+        for(int i = 1; i <= n; i++) {
+
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            for(int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+}
