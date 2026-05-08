4910de8cdffd2ec95d75d6f695fc8b0f43d6e76f
Priyanshu <pv254424@gmail.com>
2026-05-08 09:42:18 +0530

    palodromic string solved fix

--- src/TcsInterview/PalidromeString.java
+++ src/TcsInterview/PalidromeString.java
@@ -2,21 +2,33 @@ package TcsInterview;
 
 public class PalidromeString {
     public static void main(String[] args) {
-        String s = "racecar";
+        String s = "hello";
         int n = s.length();
-        boolean palindrome = true;
-        for(int i = 0; i < n; i++) {
-            for (int j = n - 1; j > i; j--) {
-                if (s.charAt(i) != s.charAt(j)) {
-                    palindrome = false;
-                    break;
-                }
-            }
-            if (palindrome) {
-                System.out.println("Palindrome");
-            } else {
-                System.out.println("Not Palindrome");
-            }
+        int i = 0, j=n-1;
+        boolean isPalindrome = true;
+        while(i<j){
+          if(s.charAt(i)!=s.charAt(j)){
+              isPalindrome = false;
+              break;
+          }
+          i++;
+          j--;
         }
+        System.out.println(isPalindrome);
+
+
+//        for(int i = 0; i < n; i++) {
+//            for (int j = n - 1; j > i; j--) {
+//                if (s.charAt(i) != s.charAt(j)) {
+//                    palindrome = false;
+//                    break;
+//                }
+//            }
+//            if (palindrome) {
+//                System.out.println("Palindrome");
+//            } else {
+//                System.out.println("Not Palindrome");
+//            }
+//        }
     }
 }
