35872dee3a0745aefdf55b1866bb0efd57c3fd7d
Priyanshu <pv254424@gmail.com>
2026-05-08 19:29:19 +0530

    reverse an array question solved

---
+++ src/TcsInterview/ReverseArray.java
@@ -0,0 +1,19 @@
+package TcsInterview;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5};
+        int left = 0;
+        int right = arr.length-1;
+        while(left<right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        for(int ele : arr){
+            System.out.print(ele+" ");
+        }
+    }
+}
