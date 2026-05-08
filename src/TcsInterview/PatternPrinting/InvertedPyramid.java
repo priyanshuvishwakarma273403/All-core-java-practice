6be966e98aeb94821fbf802029f2d25044a7651d
Priyanshu <pv254424@gmail.com>
2026-05-08 22:01:12 +0530

    inverted pyramid pattern solved

---
+++ src/TcsInterview/PatternPrinting/InvertedPyramid.java
@@ -0,0 +1,17 @@
+package TcsInterview.PatternPrinting;
+
+public class InvertedPyramid {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j<= n-i; j++){
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j<= (2 * i - 1); j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
