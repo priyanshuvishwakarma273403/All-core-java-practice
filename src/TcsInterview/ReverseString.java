6542287953a79294e69c0bfd7de41fe8e5c5c378
Priyanshu <pv254424@gmail.com>
2026-05-08 14:11:23 +0530

    using two pointer solved the reverse string

--- src/TcsInterview/ReverseString.java
+++ src/TcsInterview/ReverseString.java
@@ -6,10 +6,20 @@ public class ReverseString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        StringBuilder reversed = new StringBuilder();
-        for(int i = str.length()-1; i>= 0 ; i--){
-            reversed.append(str.charAt(i));
+        char [] arr = str.toCharArray();
+        int left = 0; int right = str.length()-1;
+        while(left<right){
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
         }
-        System.out.println(reversed);
+        System.out.println(new String(arr));
+//        StringBuilder reversed = new StringBuilder();
+//        for(int i = str.length()-1; i>= 0 ; i--){
+//            reversed.append(str.charAt(i));
+//        }
+//        System.out.println(reversed);
     }
 }
