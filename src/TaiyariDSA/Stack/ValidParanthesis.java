0d8c79c14bdceba3b37eabff02c27fad6b277b35
Priyanshu <pv254424@gmail.com>
2026-05-11 16:09:57 +0530

    Valid paranthesis solved the problem good optimisation

---
+++ src/TaiyariDSA/Stack/ValidParanthesis.java
@@ -0,0 +1,31 @@
+package TaiyariDSA.Stack;
+
+import java.util.Stack;
+
+public class ValidParanthesis {
+
+    public static boolean isValid(String str){
+        Stack<Character> stack = new Stack<>();
+        for(char ch : str.toCharArray()){
+            if(ch == '(' || ch == '[' || ch == '{' ){
+                stack.push(ch);
+
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.pop();
+                if((ch == ')' && top != '(') || (ch == '}' && top != '{') || (ch == ']' && top != '[')){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String str = "{}[]";
+        System.out.println(isValid(str));
+    }
+}
