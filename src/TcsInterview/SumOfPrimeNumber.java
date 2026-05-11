20f6c33f42a3c49a742c8a4bd983e86242e13571
Priyanshu <pv254424@gmail.com>
2026-05-11 12:10:06 +0530

    isPrimeNumber check solved

---
+++ src/TcsInterview/SumOfPrimeNumber.java
@@ -0,0 +1,26 @@
+package TcsInterview;
+
+public class SumOfPrimeNumber {
+
+    public static boolean isPrime(int n){
+        if(n<=1) return false;
+        for(int i = 2; i * i<= n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        int sum = 0;
+        for (int i = 2; i <= n; i++) {
+            if(isPrime(i)){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+
+    }
+}
