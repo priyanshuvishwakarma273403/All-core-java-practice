79eb8132560190b7bb246475f279f7c640a4f7a8
Priyanshu <pv254424@gmail.com>
2026-05-11 11:56:18 +0530

    reverse string by words

--- src/TcsInterview/interview/TwoSumUsingMap.java
+++ src/TcsInterview/interview/TwoSumUsingMap.java
@@ -1,6 +1,7 @@
 package TcsInterview.interview;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class TwoSumUsingMap {
@@ -12,24 +13,24 @@ public class TwoSumUsingMap {
 
     // agar value return krna hoto hashset se kr skte hain
     private static int[] twoSum(int[] arr, int target) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int ele : arr){
-           int rem = target - ele;
-           if(set.contains(rem)) return new int[]{ele,rem};
-           set.add(ele);
-        }
-        return new int[]{-1,-1}; //agar target naa mila tab
+//        HashSet<Integer> set = new HashSet<>();
+//        for(int ele : arr){
+//           int rem = target - ele;
+//           if(set.contains(rem)) return new int[]{ele,rem};
+//           set.add(ele);
+//        }
+//        return new int[]{-1,-1}; //agar target naa mila tab
 
 
         //agar indices return krna hoto hashMAp
-//        HashMap<Integer, Integer> map = new HashMap<>();
-//        for(int i = 0; i< arr.length; i++){
-//            int complement = target - arr[i];
-//            if(map.containsKey(complement)){
-//                return new int[]{map.get(complement),i};
-//            }
-//            map.put(arr[i],i);
-//        }
-//        return new int[]{-1,-1};  // agar target naa mile tab
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i< arr.length; i++){
+            int complement = target - arr[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
+            }
+            map.put(arr[i],i);
+        }
+        return new int[]{-1,-1};  // agar target naa mile tab
     }
 }
