4d3687c7d0abfadd5bb221fdf85f7896f5f22a07
Priyanshu <pv254424@gmail.com>
2026-05-11 15:41:35 +0530

    intersect of array solved using user input

--- src/TcsInterview/FindIntersectionOfTwoArrays.java
+++ src/TcsInterview/FindIntersectionOfTwoArrays.java
@@ -1,18 +1,37 @@
 package TcsInterview;
 
+import java.util.Scanner;
+
 public class FindIntersectionOfTwoArrays {
     public static void main(String[] args) {
-        int [] arr1 = {1,2,6,8,3};
-        int [] arr2 = {1,2,12,6,66,78};
-        for(int i = 0; i < arr1.length; i++){
-            for(int j = 0; j < arr2.length; j++){
-                if(arr1[i] == arr2[j]){
-                    System.out.print(arr1[i] + " ");
+
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int [] a = new int[n1];
+        for(int i=0;i<n1;i++) a[i] = sc.nextInt();
+
+        int n2 = sc.nextInt();
+        int [] b = new int[n1];
+        for(int i=0;i<n2;i++) b[i] = sc.nextInt();
+
+        for(int i = 0 ; i< n1; i++){
+            for(int j = 0 ; j < n2; j++){
+                if(a[i]==b[j]){
+                    System.out.print(a[i]+" ");
                     break;
                 }
             }
         }
+//        int [] arr1 = {1,2,6,8,3};
+//        int [] arr2 = {1,2,12,6,66,78};
 
-
+//        for(int i = 0; i < arr1.length; i++){
+//            for(int j = 0; j < arr2.length; j++){
+//                if(arr1[i] == arr2[j]){
+//                    System.out.print(arr1[i] + " ");
+//                    break;
+//                }
+//            }
+//        }
     }
 }
