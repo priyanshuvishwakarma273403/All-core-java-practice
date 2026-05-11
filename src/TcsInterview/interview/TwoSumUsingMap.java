a47516389680a2fcc3c939584f3537a1c17270b8
Priyanshu <pv254424@gmail.com>
2026-05-11 10:59:02 +0530

    two sum problem by using hashset solved

--- src/TcsInterview/interview/TwoSumUsingMap.java
+++ src/TcsInterview/interview/TwoSumUsingMap.java
@@ -1,23 +1,33 @@
 package TcsInterview.interview;
 
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class TwoSumUsingMap {
     public static void main(String[] args) {
         int [] arr = {2,3,4,5,6};
         int target  = 5;
-        twoSum(arr,target);
+        System.out.println(Arrays.toString(twoSum(arr, target)));
     }
 
     private static int[] twoSum(int[] arr, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i< arr.length; i++){
-            int complement = target - arr[i];
-            if(map.containsKey(complement)){
-                return new int[]{map.get(complement),i};
-            }
-            map.put(arr[i],i);
+        HashSet<Integer> set = new HashSet<>();
+        for(int ele : arr){
+           int rem = target - ele;
+           if(set.contains(rem)) return new int[]{ele,rem};
+           set.add(ele);
         }
-        return new int[]{-1,-1};  // agar target naa mile tab
+        return new int[]{-1,-1}; //agar target naa mila tab
+
+
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        for(int i = 0; i< arr.length; i++){
+//            int complement = target - arr[i];
+//            if(map.containsKey(complement)){
+//                return new int[]{map.get(complement),i};
+//            }
+//            map.put(arr[i],i);
+//        }
+//        return new int[]{-1,-1};  // agar target naa mile tab
     }
 }
