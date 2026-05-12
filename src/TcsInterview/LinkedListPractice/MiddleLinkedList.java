063bbe83721484a4f385d0a4d849ccd5f434c4fb
Priyanshu <pv254424@gmail.com>
2026-05-12 12:53:56 +0530

    middle LL print and find

--- src/TcsInterview/LinkedListPractice/MiddleLinkedList.java
+++ src/TcsInterview/LinkedListPractice/MiddleLinkedList.java
@@ -2,6 +2,15 @@ package TcsInterview.LinkedListPractice;
 
 public class MiddleLinkedList {
 
+    static int getMiddle(Node head){
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.data;
+    }
 
     public static void printLinkedList(Node head) {
         while(head != null){
@@ -16,7 +25,16 @@ public class MiddleLinkedList {
         head.next.next = new Node(3);
         head.next.next.next = new Node(4);
         head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
+//        head.next.next.next.next.next.next = new Node(7);
+
+
         printLinkedList(head);
+        System.out.println();
+
+        int middle = getMiddle(head);
+        System.out.println("Middle linked list is: " + middle);
+
 
 
     }
