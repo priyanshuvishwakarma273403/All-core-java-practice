b97de88d7c684a75896c42757af2f8ba765fc918
Priyanshu <pv254424@gmail.com>
2026-05-13 12:49:44 +0530

    two sum problem solved using map two pointer

--- src/codewithprayutosh/twoPointer/TwoSum.java
+++ src/codewithprayutosh/twoPointer/TwoSum.java
@@ -1,32 +1,43 @@
 package codewithprayutosh.twoPointer;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
 
-    public  static int[] twoSum(int[] arr, int target){
-        int i = 0, j = arr.length-1;
-        while(i<j){
-            int sum = arr[i] + arr[j];
-            if(sum == target){
-                return new int[]{i,j};
-            }
-            else if(sum < target){
-                i++;
-            }
-            else if(sum > target){
-                j++;
-            }
-        }
-        return new int[] {-1,-1};
-    }
+//    two sum
+//    public  static int[] twoSum(int[] arr, int target){
+//        int i = 0, j = arr.length-1;
+//        while(i<j){
+//            int sum = arr[i] + arr[j];
+//            if(sum == target){
+//                return new int[]{i,j};
+//            }
+//            else if(sum < target){
+//                i++;
+//            }
+//            else if(sum > target){
+//                j++;
+//            }
+//        }
+//        return new int[] {-1,-1};
+//    }
 
     public static void main(String[] args) {
         int [] arr = {1,2,3,4,5};
         int target = 7;
 
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            int complement = target - arr[i];
+            if(map.containsKey(complement)){
+              map.get(complement);
+            }
+            map.put(arr[i], i);
+        }
 
-        System.out.println(Arrays.toString(twoSum(arr,target)));
+
+//        System.out.println(Arrays.toString(twoSum(arr,target)));
         //        while(i<j){
 //            if(arr[i]+arr[j]==target){
 //                System.out.println(arr[i] +" " +arr[j]);
