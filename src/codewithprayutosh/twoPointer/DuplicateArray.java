24ab325c23fa272a943f3883a8b5b4ae0f2e362d
Priyanshu <pv254424@gmail.com>
2026-05-13 13:31:39 +0530

    Duplicate array solved the problem using two pointer technique

---
+++ src/codewithprayutosh/twoPointer/DuplicateArray.java
@@ -0,0 +1,27 @@
+package codewithprayutosh.twoPointer;
+
+import java.util.Arrays;
+
+public class DuplicateArray {
+
+    public static int findDuplicate(int[] arr) {
+
+
+        Arrays.sort(arr);
+        int i = 0;
+        int j = 1;
+        while(j< arr.length){
+            if(arr[i] == arr[j]){
+                return arr[i];
+            }
+            i++;
+            j++;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {1,3,4,2,2};
+        System.out.println(findDuplicate(arr));
+    }
+}
