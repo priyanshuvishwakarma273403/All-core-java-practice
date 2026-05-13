38eeaf65ff10331a6e9ed8df28359ce77c0b7adc
Priyanshu <pv254424@gmail.com>
2026-05-13 17:01:58 +0530

    Triplet sum solved by two pointer solved the bugs and fixex

--- src/codewithprayutosh/twoPointer/TripletSum.java
+++ src/codewithprayutosh/twoPointer/TripletSum.java
@@ -9,22 +9,31 @@ public class TripletSum {
     public static List<List<Integer>> threeSum(int[] arr) {
         List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(arr);
-        for(int i = 0; i< arr.length-2; i++) {
+
+        for (int i = 0; i < arr.length - 2; i++) {
+
             if (i > 0 && arr[i] == arr[i - 1]) continue;
+
             int left = i + 1;
             int right = arr.length - 1;
 
             while (left < right) {
                 int sum = arr[i] + arr[left] + arr[right];
+
                 if (sum == 0) {
                     list.add(Arrays.asList(arr[i], arr[left], arr[right]));
                     left++;
                     right--;
+
+                    while (left < right && arr[left] == arr[left - 1]) left++;
+                    while (left < right && arr[right] == arr[right + 1]) right--;
                 }
-                while (left < right && arr[left] == arr[left - 1]) left++;
-                while (left < right && arr[right] == arr[right + 1]) right--;
-                if(sum < 0) left++;
-                else right--;
+                else if (sum < 0) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
             }
         }
         return list;
@@ -32,7 +41,7 @@ public class TripletSum {
 
     public static void main(String[] args) {
         int [] arr = {-1,0,1,2,-1,-4};
-        System.out.println(Arrays.toString(new List[]{threeSum(arr)}));
+        System.out.println(threeSum(arr));
 
 //        for(int i = 0; i < arr.length; i++){
 //            for(int j = i+1; j < arr.length; j++){
