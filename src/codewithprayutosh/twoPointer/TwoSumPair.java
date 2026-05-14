b81344e3dcbb3ba84c7b7d81f85946cc7de19921
Priyanshu <pv254424@gmail.com>
2026-05-14 13:13:30 +0530

    two pointer solved

--- src/codewithprayutosh/twoPointer/TwoSumPair.java
+++ src/codewithprayutosh/twoPointer/TwoSumPair.java
@@ -1,9 +1,39 @@
 package codewithprayutosh.twoPointer;
 
 public class TwoSumPair {
-    public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6};
-        int target = 6;
 
+    public static void printUniquePairs(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) {
+                System.out.println(arr[left] + " " + arr[right]);
+
+                int leftValue = arr[left];
+                int rightValue = arr[right];
+
+                // skip duplicates
+                while (left < right && arr[left] == leftValue) {
+                    left++;
+                }
+                while (left < right && arr[right] == rightValue) {
+                    right--;
+                }
+
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5, 7, 11};
+        int target = 9;
+        printUniquePairs(arr, target);
     }
 }
