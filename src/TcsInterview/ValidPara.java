2175d1893389557d92f9f4c31ef1a57197792aeb
Priyanshu <pv254424@gmail.com>
2026-05-15 23:37:04 +0530

    valid paranthesis solved the problem

---
+++ src/TcsInterview/ValidPara.java
@@ -0,0 +1,25 @@
+package TcsInterview;
+
+import java.util.Stack;
+
+public class ValidPara {
+
+    public static boolean isValid(String str){
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : str.toCharArray()){
+            if(ch == '(') stack.push(')');
+            else if(ch == '[') stack.push(']');
+            else if(ch == '{') stack.push('}');
+            else{
+                if(stack.isEmpty() || stack.pop() != ch) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String str = "()[{}";
+        System.out.println(isValid(str));
+    }
+}
