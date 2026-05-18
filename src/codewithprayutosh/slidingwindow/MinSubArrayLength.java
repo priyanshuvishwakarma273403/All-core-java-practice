f960c0b6c84b657bdd21aa0d71977ae6a4722251
Priyanshu <pv254424@gmail.com>
2026-05-18 11:33:59 +0530

    min subarray length problem solved bugs fix

--- src/codewithprayutosh/slidingwindow/MinSubArrayLength.java
+++ src/codewithprayutosh/slidingwindow/MinSubArrayLength.java
@@ -1,7 +1,7 @@
 package codewithprayutosh.slidingwindow;
 
 // Example 1:
-//Input: target = 7, nums = [2,3,1,2,4,3]
+//Input: target = 7, arr = [2,3,1,2,4,3]
 //Output: 2
 //Explanation: The subarray [4,3] has the minimal length under the problem constraint.
 
