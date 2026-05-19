529d8ab870416da22b8dd13a8b62fb65c05d04de
Priyanshu <pv254424@gmail.com>
2026-05-19 12:05:45 +0530

    middle linked list solved

--- src/codewithprayutosh/slowAndFast/MiddleLinkedList.java
+++ src/codewithprayutosh/slowAndFast/MiddleLinkedList.java
@@ -10,17 +10,32 @@ class ListNode{
 }
 
 public class MiddleLinkedList {
+
+    public static ListNode findMiddleNode(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null &&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+
+
     public static void main(String[] args) {
         ListNode head=new ListNode(1);
         head.next=new ListNode(2);
         head.next.next=new ListNode(3);
         head.next.next.next=new ListNode(4);
         head.next.next.next.next=new ListNode(5);
+        head.next.next.next.next.next=new ListNode(6);
 
-        while(head != null){
-            System.out.print(head.data+" ");
-            head=head.next;
-        }
+        System.out.println(findMiddleNode(head).data);
+
+//        while(head != null){
+//            System.out.print(head.data+" ");
+//            head=head.next;
+//        }
 
     }
 }
