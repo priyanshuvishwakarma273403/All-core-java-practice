0aef16fed4fa499f5f2b006ae32ca15309889b51
Priyanshu <pv254424@gmail.com>
2026-05-26 15:25:57 +0530

    remove all adjacent element fixed the problem

---
+++ src/codewithprayutosh/stack/RemoveAllAdjacent.java
@@ -0,0 +1,31 @@
+package codewithprayutosh.stack;
+
+import java.util.Stack;
+
+public class RemoveAllAdjacent {
+
+    public static String removeDuplicates(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(!stack.isEmpty() && stack.peek() == ch){
+                stack.pop();
+            }else{
+                stack.push(ch);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(char ch : stack){
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+
+
+    public static void main(String[] args) {
+        String s = "abbaca";
+        System.out.println(removeDuplicates(s));
+    }
+}
