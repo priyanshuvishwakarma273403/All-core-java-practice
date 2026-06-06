9bc12151fff500bbdd396b1b950b4fedb987f0bc
Priyanshu <pv254424@gmail.com>
2026-06-06 23:36:05 +0530

    factorial practice cwh file

---
+++ src/Practice/cwh201.java
@@ -0,0 +1,15 @@
+package Practice;
+
+
+public class cwh201 {
+    public static int factorial(int n){
+        if( n == 0 || n == 1) return n;
+        return n * factorial(n-1);
+
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(factorial(n));
+    }
+}
